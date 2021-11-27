9c74ff10b14a5ddb082f597bbeac6c039db08f8d
Andrey Khakimov <khakimov013@gmail.com>
2021-11-27 22:23:40 +0300

    Initial Commit

---
+++ PostalCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PostalCode
+//
+//  Created by Andrey Khakimov on 27.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
